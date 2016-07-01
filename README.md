abca888669d56834d4b7536da3027d91ebb7e9b1
Kurt Spindler <kurt@neurocurious.ai>
2016-06-30 21:54:10 -0400

    package setup

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Albatross
 
-I wanted to see how simple it would be to make an async web framework for Python3.5 only.
+I wanted to see how simple it is to make an modern async web framework. (python3.5 only)
 
 It turns out - it's dead simple.
 
+## Features
+
+The entire framework is 4 files at the moment:
+
+- status_codes.py - blatantly copied from Falcon, because they did such a great job with that framework.
+- server.py - the web server you instantiate, add routes & handlers, and allows you to serve
+- request.py - a web request object
+- response.py - a web response object
+
+Each of those is no less than 100 or so lines.
+
+## Todo
+
+- tests
     python3 example_app.py
---
+++ setup.py
@@ -0,0 +1,74 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='albatross3',
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='A modern async python3 web framework',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/kespindler/albatross',
+
+    # Author details
+    author='Kurt Spindler',
+    author_email='kespindler@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='web http server async',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests', 'examples']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=[],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': [],
+        'test': [],
+    },
+)

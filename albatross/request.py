f0064b68060e24727e37ecc86709219f88dc5720
Kurt Spindler <kurt@neurocurious.ai>
2016-07-02 02:56:10 -0400

    smarter request parsing

--- albatross/request.py
+++ albatross/request.py
@@ -1,4 +1,7 @@
 import urllib.parse as parse
+import ujson as json
+import re
+
 
 
 class Request:
@@ -10,6 +13,7 @@ class Request:
         query (dict): dict of the request query
         body (str): Request body
         args (dict): Dictionary of named parameters in route regex
+        form (dict): Dictionary of body parameters
     """
     def __init__(self, method, path, query_string, body, args, headers):
         self.method = method
@@ -23,7 +27,21 @@ class Request:
         if body:
             self._parse_body()
 
-    def _parse_body(self):
-        if self.headers['Content-Type'] == 'application/x-www-form-urlencoded':
-            self.form = parse.parse_qs(self.body)
+    def _parse_form(self):
+        content_type = self.headers['Content-Type']
+        marker = 'boundary='
+        index = content_type.find(marker)
+        boundary = '--' + content_type[index + len(marker):]
+        values = {}
+        for group in re.findall('name=\"([a-zA-Z0-9-]+)\"\r\n\r\n(.*?)\r\n' + boundary, self.body,  re.MULTILINE):
+            values[group[0]] = group[1]
+        return values
 
+    def _parse_body(self):
+        content_type = self.headers['Content-Type']
+        if content_type == 'application/x-www-form-urlencoded':
+            self.form = parse.parse_qs(self.body)
+        elif content_type == 'application/json':
+            self.form = json.loads(self.body)
+        elif content_type.startswith('multipart/form-data'):
+            self.form = self._parse_form()

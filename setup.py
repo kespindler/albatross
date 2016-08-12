19f1f5e12e62508415a541ade00aeaa4efdaeb6e
Kurt Spindler <kurt@neurocurious.ai>
2016-08-12 14:44:12 -0400

    add httptools to requires. bump package to 0.5.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.1',
+    version='0.5.0',
     description='A modern async python3 web framework',
     long_description=long_description,
     url='https://github.com/kespindler/albatross',
@@ -37,6 +37,9 @@ setup(
     ],
     keywords='web http server async',
     packages=['albatross'],
+    install_requires=[
+        'httptools',
+    ],
     extras_require={
         'ujson': ['ujson']
     },
